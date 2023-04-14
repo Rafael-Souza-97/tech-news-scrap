b64f100041c5ca762ba5591b240a59a2c65b57f7
Rafael Souza <souza_rafael.97@outlook.com>
2023-04-14 15:20:23 -0300

    feat: Req 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,13 @@
+from tech_news.database import db
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    categories = db.news.aggregate([
+        {"$unwind": "$category"},
+        {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+    ])
+    sort_categories = sorted(categories, key=lambda x: (-x['count'], x['_id']))
+    pop_categories = [category['_id'] for category in sort_categories[:5]]
+    print('LOG --->', pop_categories)
+    return pop_categories
