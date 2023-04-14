1225473d120eb18d1f8a4372faf0851efeeafd78
Rafael Souza <souza_rafael.97@outlook.com>
2023-04-14 14:44:58 -0300

    feat: Req 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -25,4 +25,8 @@ def search_by_date(date):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    list = []
+    search = db.news.find({'category': {'$regex': category, '$options': 'i'}})
+    for value in search:
+        list.append((value['title'], value['url']))
+    return list
